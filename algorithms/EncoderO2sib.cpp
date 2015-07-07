6b98cf30cef9419fef2a0749f48b271a68419e27
zzs <zzsfornlp@163.com>
2015-07-07 17:11:11 +0800

    v1.9.1c, back to max's encoder
    -- back to max's encoder to see whether it's ok.
    -- ready for o2sib, considering using max's.

---
+++ algorithms/EncoderO2sib.cpp
@@ -0,0 +1,12 @@
+/*
+ * EncoderO2sib.cpp
+ *
+ *  Created on: 2015Äê7ÔÂ7ÈÕ
+ *      Author: zzs
+ */
+
+#include "EisnerO2sib.h"
+
+
+
+
--- algorithms/Helper.h
+++ algorithms/Helper.h
@@ -12,7 +12,8 @@
 const double MINUS_LOG_EPSILON=50;
 
 // log(exp(x) + exp(y));
-inline double logsumexp(double x, double y) {
+inline double logsumexp(double x, double y, bool flg) {
+	if (flg) return y;  // init mode
 	const double vmin = (x>y)?y:x;
 	const double vmax = (x<y)?y:x;
 	if (vmax > vmin + MINUS_LOG_EPSILON) {
